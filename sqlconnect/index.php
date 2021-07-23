24aeab246fdf3bf25a8e432ba7f68f7f63c2df01
Vinícius.Lessa <vinicius.lessa33@Outlook.com.br>
2021-07-23 11:48:39 -0300

    None

--- sqlconnect/index.php
+++ sqlconnect/index.php
@@ -4,6 +4,19 @@
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
     // header('Content-type: application/json; charset=UTF-8');
 
+    require_once 'classes/Class.Crud.php';
+    
+    # Estabelece Conexão com o BANCO DE DADOS
+    
+    // Class.Conexao.php
+    $pdo = Conexao::getConexao();
+    
+    // Class.Class.Crud.php
+    Crud::setConexao($pdo);
+    
+    // Parâmetro passado pela URL
+    $uri = basename($_SERVER['REQUEST_URI']);
+
     echo "Olá Mundo!";
 
     if ($_SERVER['REQUEST_METHOD'] == 'PUT'):
