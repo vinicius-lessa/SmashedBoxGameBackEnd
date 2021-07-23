f9e0b4463eb6f89b5acb58e058ea10e8573137dd
Vinícius.Lessa <vinicius.lessa33@Outlook.com.br>
2021-07-23 16:19:49 -0300

    Mudança DATABASE para HEROKU

--- sqlconnect/Class.Conexao.php
+++ sqlconnect/Class.Conexao.php
@@ -1,21 +1,28 @@
 <?php
 
-// VERSÃO LOCAL
+// LOCAL - MAMP
+    // define('HOST'       , '127.0.0.1'       );
+    // define('DBNAME'     , 'unbreakble_box'  );
+    // define('USER'       , 'root'            );
+    // define('PASSWORD'   , 'root'            );
 
-// define('HOST'       , '127.0.0.1'       );
-// define('DBNAME'     , 'unbreakble_box'  );
-// define('USER'       , 'root'            );
-// define('PASSWORD'   , 'root'            );
+// WEB - INITYFREE
+    // define('HOST'       , 'sql200.epizy.com'        );
+    // define('DBNAME'     , 'epiz_29215994_smashed'   );
+    // define('USER'       , 'epiz_29215994'           );
+    // define('PASSWORD'   , 'Hy55QIMDeEn'             );
 
-// define('HOST'       , 'sql200.epizy.com'        );
-// define('DBNAME'     , 'epiz_29215994_smashed'   );
-// define('USER'       , 'epiz_29215994'           );
-// define('PASSWORD'   , 'Hy55QIMDeEn'             );
+// WEB - DB 4 FREE
+    // define('HOST'       , 'db4free.net'    );
+    // define('DBNAME'     , 'smashedboxdb'   );
+    // define('USER'       , 'root_smashed'   );
+    // define('PASSWORD'   , 'TAunrBApLYGLru4');
 
-define('HOST'       , 'db4free.net'    );
-define('DBNAME'     , 'smashedboxdb'   );
-define('USER'       , 'root_smashed'   );
-define('PASSWORD'   , 'TAunrBApLYGLru4');
+// WEB - HEROKU (smashed-box-beckend)
+    define('HOST'       , 'us-cdbr-east-04.cleardb.com' );
+    define('DBNAME'     , 'heroku_f8611d16df426e6'      );
+    define('USER'       , 'bc5758a825e4d6'              );
+    define('PASSWORD'   , 'aa7c67cc'                    );
  
 class Conexao
 {
