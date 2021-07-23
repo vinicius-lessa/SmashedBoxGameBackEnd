5e4d59395c8d5a2d4df4513f3927254b1c4f7ac3
Vinícius.Lessa <vinicius.lessa33@Outlook.com.br>
2021-07-23 11:44:03 -0300

    None

--- sqlconnect/highscore.php
+++ sqlconnect/highscore.php
@@ -3,7 +3,7 @@
         
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    header('Content-type: application/json; charset=UTF-8');
+    // header('Content-type: application/json; charset=UTF-8');
     
     require_once 'classes/Class.Crud.php';
     
--- sqlconnect/index.php
+++ sqlconnect/index.php
@@ -2,7 +2,7 @@
 
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    header('Content-type: application/json; charset=UTF-8');
+    // header('Content-type: application/json; charset=UTF-8');
 
     echo "Olá Mundo!";
 
