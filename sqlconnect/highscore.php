7a4a403b68d7383b94180f1a5060bc2230d4478b
Vinícius.Lessa <vinicius.lessa33@Outlook.com.br>
2021-07-23 12:40:20 -0300

    Changing Header php

--- sqlconnect/highscore.php
+++ sqlconnect/highscore.php
@@ -3,7 +3,7 @@
         
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    // header('Content-type: application/json; charset=UTF-8');
+    header('Content-type: application/json; charset=UTF-8');
     
     require_once 'Class.Crud.php';
     require_once 'Class.Conexao.php';
