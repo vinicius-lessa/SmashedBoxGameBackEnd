23e4c07cba347f441f046f08389a3b8c248805ba
Vinícius.Lessa <vinicius.lessa33@Outlook.com.br>
2021-08-18 21:20:42 -0300

    Changes on the Header allow

--- sqlconnect/highscore.php
+++ sqlconnect/highscore.php
@@ -3,6 +3,7 @@
         
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: *");
+    header("Access-Control-Allow-Headers: X-Requested-With");
     header("Access-Control-Allow-Credentials: true");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header("Content-type: application/json; charset=UTF-8");    
